e1e37179257a065073e4f95761827345ffbb5a6b
zxj <474339628@qq.com>
2022-10-08 18:09:11 +0800

    增加MapUtil说明

--- src/main/java/org/apache/ibatis/util/MapUtil.java
+++ src/main/java/org/apache/ibatis/util/MapUtil.java
@@ -27,6 +27,12 @@ public class MapUtil {
    *
    * @see <a href="https://bugs.openjdk.java.net/browse/JDK-8161372">https://bugs.openjdk.java.net/browse/JDK-8161372</a>
    */
+  
+  /**
+   * 此工具包的出现是为了解决使用java.util.concurrent.ConcurrentHashMap中的computeIfAbsent方法时而出现的性能瓶颈问题，简单描述即
+   * ConcurrentHashMap中的computeIfAbsent方法在寻找key对应的value时，则会进入由synchronized修饰的寻找value的代码块，而这个性能在
+   * 高并发的情况下会导致严重的问题——线程阻塞，而由于ConcurrentHashMap是线程安全类，所以可以直接获取key对应的value，从而产生此工具类。
+   */
   public static <K, V> V computeIfAbsent(Map<K, V> map, K key, Function<K, V> mappingFunction) {
     V value = map.get(key);
     if (value != null) {
@@ -37,6 +43,10 @@ public class MapUtil {
 
   /**
    * Map.entry(key, value) alternative for Java 8.
+   */
+  
+  /**
+   * 创建一个与给定的key以及value相同的键值对，且key以及value都不可变。
    */
   public static <K, V> Entry<K, V> entry(K key, V value) {
     return new AbstractMap.SimpleImmutableEntry<>(key, value);
